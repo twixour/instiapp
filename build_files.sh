dbe1f72caa795e59780a5bda5fb12c69da37ffd6
twixour <twixour@gmail.com>
2023-06-21 17:35:28 +0530

    build-sh1

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
-python3 - m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
-python3 manage.py makemigrations
-python3 manage.py migrate
+python - m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
+python manage.py makemigrations
+python manage.py migrate
 echo "BUILD END"
