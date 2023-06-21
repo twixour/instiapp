731cafccc5292f5f1c7c0ca12735e47b5515eef5
twixour <twixour@gmail.com>
2023-06-21 09:49:45 +0530

    require

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+#import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,12 +84,39 @@ WSGI_APPLICATION = 'core.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'usersDB', 
+#         'USER': 'default', 
+#         'PASSWORD': 'r5SDAkvuN0jR',
+#         'HOST': 'ep-black-bar-039461-pooler.us-east-1.postgres.vercel-storage.com', 
+#         'PORT': '5432',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'twixie',
+        'USER':'postgres',
+        'PASSWORD':'gameover',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
+
+
+
 
 
 # Password validation
@@ -146,3 +173,8 @@ REST_FRAMEWORK = {
 
 AUTH_USER_MODEL = "user.User"
 CORS_ALLOW_ALL_ORIGINS = True
+
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# MEDIA_URLS ='/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
