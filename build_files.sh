06f16bde159cdfa6fb7f81c0552394e1809d4ef1
twixour <twixour@gmail.com>
2023-06-21 10:01:55 +0530

    set4

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
 echo "BUILD START"
 python3.11 - m pip install -r requirements.txt
 python3.11 manage.py collectstatic --noinput --clear
+python3.11 manage.py makemigrations
+python3.11 manage.py migrate
